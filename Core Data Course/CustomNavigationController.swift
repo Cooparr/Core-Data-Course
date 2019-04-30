96a4b85e313cc04c48312677901e312b0e4070ab
Alex Cooper <cooparr@hotmail.co.uk>
2019-04-30 16:10:17 +0100

    Lesson 2

---
+++ Core Data Course/CustomNavigationController.swift
@@ -0,0 +1,15 @@
+//
+//  CustomNavigationController.swift
+//  Core Data Course
+//
+//  Created by Alex Cooper on 30/04/2019.
+//  Copyright © 2019 Alexander Cooper. All rights reserved.
+//
+
+import UIKit
+
+class CustomNavigationController: UINavigationController {
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return .lightContent
+    }
+}
--- Core Data Course/ViewController.swift
+++ Core Data Course/ViewController.swift
@@ -10,11 +10,28 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    //MARK:- View Did Load
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        view.backgroundColor = .white
+        setupNavigationStyle()
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(image: #imageLiteral(resourceName: "plusImage"), style: .plain, target: self, action: #selector(handleAddCompany))
     }
-
-
+    
+    @objc func handleAddCompany() {
+        print("Adding Company!")
+    }
+    
+    //MARK:- Setup Navigation Bar Style
+    fileprivate func setupNavigationStyle() {
+        let lightRed = UIColor.init(red: 247/255, green: 66/255, blue: 82/255, alpha: 1)
+        navigationItem.title = "Companies"
+        navigationController?.navigationBar.isTranslucent = false
+        navigationController?.navigationBar.barTintColor = lightRed
+        navigationController?.navigationBar.prefersLargeTitles = true
+        navigationController?.navigationBar.largeTitleTextAttributes = [NSAttributedString.Key.foregroundColor : UIColor.white]
+    }
 }
 
