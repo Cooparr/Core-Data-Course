39dec50d8978b1e32fd401438f9ae5c0c031e468
Alex Cooper <cooparr@hotmail.co.uk>
2019-04-30 16:59:54 +0100

    Lesson 3

---
+++ Core Data Course/CustomColors.swift
@@ -0,0 +1,16 @@
+//
+//  CustomColors.swift
+//  Core Data Course
+//
+//  Created by Alex Cooper on 30/04/2019.
+//  Copyright © 2019 Alexander Cooper. All rights reserved.
+//
+
+import UIKit
+
+extension UIColor {
+    static let redColor = UIColor(red: 247/255, green: 66/255, blue: 82/255, alpha: 1)
+    static let tealColor = UIColor(red: 48/255, green: 164/255, blue: 182/255, alpha: 1)
+    static let darkBlueColor = UIColor(red: 9/255, green: 45/255, blue: 64/255, alpha: 1)
+    static let lightBlueColor = UIColor(red: 218/255, green: 235/255, blue: 243/255, alpha: 1)
+}
--- Core Data Course/ViewController.swift
+++ Core Data Course/ViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
 
     //MARK:- View Did Load
     override func viewDidLoad() {
@@ -18,6 +18,11 @@ class ViewController: UIViewController {
         setupNavigationStyle()
         
         navigationItem.rightBarButtonItem = UIBarButtonItem(image: #imageLiteral(resourceName: "plusImage"), style: .plain, target: self, action: #selector(handleAddCompany))
+        
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cellId")
+        tableView.backgroundColor = .darkBlueColor
+        tableView.separatorColor = .white
+        tableView.tableFooterView = UIView()
     }
     
     @objc func handleAddCompany() {
@@ -26,12 +31,36 @@ class ViewController: UIViewController {
     
     //MARK:- Setup Navigation Bar Style
     fileprivate func setupNavigationStyle() {
-        let lightRed = UIColor.init(red: 247/255, green: 66/255, blue: 82/255, alpha: 1)
         navigationItem.title = "Companies"
         navigationController?.navigationBar.isTranslucent = false
-        navigationController?.navigationBar.barTintColor = lightRed
+        navigationController?.navigationBar.barTintColor = .redColor
         navigationController?.navigationBar.prefersLargeTitles = true
+        navigationController?.navigationBar.titleTextAttributes = [NSAttributedString.Key.foregroundColor : UIColor.white]
         navigationController?.navigationBar.largeTitleTextAttributes = [NSAttributedString.Key.foregroundColor : UIColor.white]
     }
+    
+    override func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        let headerView = UIView()
+        headerView.backgroundColor = .lightBlueColor
+        return headerView
+    }
+    
+    override func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 50
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cellId", for: indexPath)
+        
+        cell.backgroundColor = .tealColor
+        cell.textLabel?.text = "Companies"
+        cell.textLabel?.textColor = .white
+        cell.textLabel?.font = UIFont.boldSystemFont(ofSize: 16)
+        
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 8 
+    }
 }
-
